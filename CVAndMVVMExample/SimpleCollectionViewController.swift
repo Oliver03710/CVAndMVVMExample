204b007249f084a210840c5386c8daf10d3c68ab
oliver <jhee2619@icloud.com>
2022-10-18 11:56:19 +0900

    Set Collection Cells with Content Configurations

--- CVAndMVVMExample/SimpleCollectionViewController.swift
+++ CVAndMVVMExample/SimpleCollectionViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
+class User {
+    let name: String
+    let age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
 class SimpleCollectionViewController: UICollectionViewController {
     
     // MARK: - Properties
     
-    var list = ["닭곰탕", "삼계탕", "들기름김", "삼분카레", "콘소메 치킨"]
+//    var list = ["닭곰탕", "삼계탕", "들기름김", "삼분카레", "콘소메 치킨"]
+    
+    var list = [
+        User(name: "뽀로로", age: 10),
+        User(name: "홍길동", age: 15),
+        User(name: "둘리", age: 25),
+        User(name: "사탕", age: 33),
+        User(name: "고길동", age: 40)
+    ]
+    
+    
     
     // cellForItemAt 전에 생성되야 함. -> Register 코드와 유사
-    var cellRegistration: UICollectionView.CellRegistration<UICollectionViewListCell, String>!
+    var cellRegistration: UICollectionView.CellRegistration<UICollectionViewListCell, User>!
     
     
     // MARK: - Init
@@ -22,6 +42,8 @@ class SimpleCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         // 14+ 컬렉션뷰를 테이블뷰 스타일처럼 사용가능 (List Configuration)
         var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        configuration.showsSeparators = false
+        configuration.backgroundColor = .systemBrown
         
         let layout = UICollectionViewCompositionalLayout.list(using: configuration)
         
@@ -29,12 +51,21 @@ class SimpleCollectionViewController: UICollectionViewController {
         
         cellRegistration = UICollectionView.CellRegistration { cell, indexPath, itemIdentifier in
             
-            var content = cell.defaultContentConfiguration()
+            var content = UIListContentConfiguration.valueCell()
+//            cell.defaultContentConfiguration()
             
-            content.text = itemIdentifier
-            content.image = UIImage(systemName: "person.fill")
+            content.text = itemIdentifier.name
+            content.textProperties.color = .systemBlue
+            
+            content.secondaryText = "\(itemIdentifier.age)"
+            content.prefersSideBySideTextAndSecondaryText = false
+            content.textToSecondaryTextVerticalPadding = 20
+            
+            content.image = indexPath.item < 3 ? UIImage(systemName: "person.fill") : UIImage(systemName: "star.fill")
+            content.imageProperties.tintColor = .black
             
             cell.contentConfiguration = content
+            
             
         }
     }
