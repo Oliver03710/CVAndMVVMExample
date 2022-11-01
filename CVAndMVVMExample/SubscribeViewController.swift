cda7e9c4133425ee8a0b59128afbfddfae8833a6
Oliver03710 <jhee2619@gmail.com>
2022-11-01 10:19:04 +0900

    RxAlamofire & RxDataSource Test

--- CVAndMVVMExample/SubscribeViewController.swift
+++ CVAndMVVMExample/SubscribeViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
+import RxAlamofire
 import RxCocoa
+import RxDataSources
 import RxSwift
 
 class SubscribeViewController: UIViewController {
@@ -16,14 +18,35 @@ class SubscribeViewController: UIViewController {
     
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     let disposeBag = DisposeBag()
+    
+    lazy var dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Int>> { dataSource, tableView, indexPath, item in
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = "\(item)"
+        return cell
+    }
     
     
     // MARK: - Init
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        testRxAlamofire()
+        testRxDataSource()
+        
+        Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            .skip(3)
+            .filter { $0 % 2 == 0 }
+//            .debug()
+            .map { $0 * 2 }
+            .subscribe { value in
+//                print("\(value)")
+            }
+            .disposed(by: disposeBag)
+
         
         // 첫번째 방법
         button.rx.tap
@@ -59,6 +82,7 @@ class SubscribeViewController: UIViewController {
         
         // 다섯번째 방법: operator로 데이터의 stream 조작
         button.rx.tap
+            // .debug()    // Print 해주는 명령어 -> 출시 후 그리 필요하지 않은 부분
             .map { "안녕 반가워" }
             .bind(to: label.rx.text)
             .disposed(by: disposeBag)
@@ -68,6 +92,38 @@ class SubscribeViewController: UIViewController {
             .map { "안녕 반가워" }
             .asDriver(onErrorJustReturn: "")
             .drive(label.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    func testRxAlamofire() {
+        // Success, Error -> <Single>
+        let url = APIKeys.searchURL + "apple"
+        request(.get, url, headers: ["Authorization": APIKeys.authorization])
+            .data()
+            .decode(type: SearchPhoto.self, decoder: JSONDecoder())
+            .subscribe(onNext: { value in
+                print(value.results[0].likes)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func testRxDataSource() {
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        dataSource.titleForHeaderInSection = { dataSource, index in
+            return dataSource.sectionModels[index].model
+        }
+        
+        Observable.just([
+            SectionModel(model: "title1", items: [1, 2, 3]),
+            SectionModel(model: "title2", items: [1, 2, 3]),
+            SectionModel(model: "title3", items: [1, 2, 3])
+        ])
+            .bind(to: tableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
     }
 }
